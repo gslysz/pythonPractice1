9d29fce8101626f2e7f64d6911ee623caa133c70
gslysz <gordons@cadwell.com>
2018-10-29 09:27:09 -0700

    minor

--- PythonApplication1/PythonApplication1/ReadPerfMonCsv.py
+++ PythonApplication1/PythonApplication1/ReadPerfMonCsv.py
@@ -21,12 +21,16 @@ def readCsvPandas(filename):
     return df
 
 
-def MakeChartForBytesUsed(filename):
+def MakeChartForBytesUsed(filename, title = "Bytes", xlim=(0,1000), ylim=(0,5e9)):
     df = readCsvPandas(filename)
 
     yvals = df[df.columns[1]]
     indexVals= list(range(0, len(yvals), 1))
     xvals = [i * 0.5 for i in indexVals]   # points are taken every 30 sec.  So this time in minutes.
+
+    #yvals=  yvals[0:1600]
+    #xvals = xvals[0:1600]
+
 
     slope, intercept, r_value, p_value, std_err = linregress(xvals,yvals)
     slopeMb = slope / 1024 / 1024
@@ -40,26 +44,52 @@ def MakeChartForBytesUsed(filename):
     plt.plot(xvals, yvals)
     plt.plot(xvals, trendline)
 
-    ymax= 5e9
-
-    plt.ylim(0, 5e9)
-    plt.xlim(0, 1000)
+    ymax= ylim[1]
+    plt.ylim(ylim[0], ylim[1])
+    plt.xlim(xlim[0], xlim[1])
 
     plt.xlabel("time (min)")
-    plt.ylabel("num bytes")
+    plt.ylabel("bytes")
+    plt.title(title)
     plt.annotate(slopeText, xy=(0,ymax*0.9))
     plt.savefig(pngFileName)
+    print(f'Saved graph to file: {pngFileName}')
+    return pngFileName
 
 
-filename1= "\\\\taz\\Users\\Gord\\DataAnalysis\\2018\\2018_10_17 PerfMon LiveReview\\ArcNoSpectrogram.csv"
-filename2= "\\\\taz\\Users\\Gord\\DataAnalysis\\2018\\2018_10_17 PerfMon LiveReview\\ArcWithSpectrogram.csv"
-filename3= "\\\\taz\\Users\\Gord\\DataAnalysis\\2018\\2018_10_17 PerfMon LiveReview\\ArcWithSpectrogram_15MinWindow.csv"
-filename4= "\\\\taz\\Users\\Gord\\DataAnalysis\\2018\\2018_10_17 PerfMon LiveReview\\ArcWithSpectrogram_WindowCollapsed.csv"
 
-filenames= [filename1,filename2,filename3,filename4]
 
-for f in filenames:
-    MakeChartForBytesUsed(f)
+def MakeChartThreads(filename, title = "Threads", xlim=(0,1000), ylim=(0,500)):
+    df = readCsvPandas(filename)
+
+    yvals = df[df.columns[2]]
+    indexVals= list(range(0, len(yvals), 1))
+    xvals = [i * 0.5 for i in indexVals]   # points are taken every 30 sec.  So this time in minutes.
+    
+    pngFileName= filename.replace(".csv","_threads.png")
+    plt.plot(xvals, yvals)
+    
+    ymax= ylim[1]
+    plt.ylim(ylim[0], ylim[1])
+    plt.xlim(xlim[0], xlim[1])
+
+    plt.xlabel("time (min)")
+    plt.ylabel("threads")
+    plt.title(title)
+    plt.savefig(pngFileName)
+    print(f'Saved graph to file: {pngFileName}')
+    return pngFileName
+
+
+#filename1= "\\\\taz\\Users\\Gord\\DataAnalysis\\2018\\2018_10_17 PerfMon LiveReview\\ArcNoSpectrogram.csv"
+#filename2= "\\\\taz\\Users\\Gord\\DataAnalysis\\2018\\2018_10_17 PerfMon LiveReview\\ArcWithSpectrogram.csv"
+#filename3= "\\\\taz\\Users\\Gord\\DataAnalysis\\2018\\2018_10_17 PerfMon LiveReview\\ArcWithSpectrogram_15MinWindow.csv"
+#filename4= "\\\\taz\\Users\\Gord\\DataAnalysis\\2018\\2018_10_17 PerfMon LiveReview\\ArcWithSpectrogram_WindowCollapsed.csv"
+
+#filenames= [filename1,filename2,filename3,filename4]
+
+#for f in filenames:
+#    MakeChartForBytesUsed(f)
 
 
 
---
+++ PythonApplication1/PythonApplication1/Scripts1.py
@@ -0,0 +1,17 @@
+import ReadPerfMonCsv as reader
+import os
+
+print("Starting...")
+filename = "\\\\taz\\Users\\Gord\\DataAnalysis\\2018\\2018_10_17 PerfMon LiveReview\\Arc250Hz1sEpochSpectrogram.csv"
+filename = "\\\\taz\\Users\\Gord\\DataAnalysis\\2018\\2018_10_17 PerfMon LiveReview\\Arc250Hz4sEpochSpectrogram.csv"
+graphFilename1= reader.MakeChartForBytesUsed(filename, ylim=(0,5e9))
+graphFileName2= reader.MakeChartThreads(filename)
+
+#os.startfile(graphFilename1)
+#os.startfile(graphFileName2)
+
+print("Done!")
+
+
+
+
