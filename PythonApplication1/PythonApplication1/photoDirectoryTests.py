19062e30579ea8ddfe26f45b18620031e16de5a9
gslysz <gordons@cadwell.com>
2018-12-28 11:23:50 -0800

    now copying to month folder

--- PythonApplication1/PythonApplication1/photoDirectoryTests.py
+++ PythonApplication1/PythonApplication1/photoDirectoryTests.py
@@ -1,7 +1,4 @@
 import PythonApplication1.PythonApplication1.photoDirectoryUtils as utils
-import os
 
 year_folderPath = "C:/gord/temp"
-#utils.create_month_folders(year_folderPath)
-
 utils.organize_files(year_folderPath, year_folderPath)
