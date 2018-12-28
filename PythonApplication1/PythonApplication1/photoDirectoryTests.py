e053667718ff763659a29f61722ff1904f129792
gslysz <gordons@cadwell.com>
2018-12-28 15:49:21 -0800

    can optionally create month folders; will now create year folders; can start from commandline

--- PythonApplication1/PythonApplication1/photoDirectoryTests.py
+++ PythonApplication1/PythonApplication1/photoDirectoryTests.py
@@ -1,4 +1,4 @@
 import PythonApplication1.PythonApplication1.photoDirectoryUtils as utils
 
-year_folderPath = "C:/gord/temp"
+year_folderPath = "C:/Gord/temp4"
 utils.organize_files(year_folderPath, year_folderPath)
--- PythonApplication1/PythonApplication1/photoDirectoryUtils.py
+++ PythonApplication1/PythonApplication1/photoDirectoryUtils.py
@@ -2,6 +2,45 @@ import os
 import datetime
 import calendar
 import shutil
+import sys
+
+
+def organize_files(source_folder, target_base_folder):
+    source_exists = os.path.exists(source_folder)
+    if not source_exists:
+        msg = "Cannot organize files. Source path does not exist. Source=" + source_folder
+        print(msg)
+        return
+    if not os.path.exists(target_base_folder):
+        msg = "Created folder: " + target_base_folder
+        print(msg)
+        os.mkdir(target_base_folder)
+
+    all_files = os.listdir(source_folder)
+    filtered_files = []
+
+    for f in all_files:
+        f_lower = f.lower()
+        if (f_lower.endswith(".jpg")
+                or f_lower.endswith(".mp4")
+                or f_lower.endswith(".mov")
+                or f_lower.endswith(".png")):
+            filtered_files.append(f_lower)
+
+    for f in filtered_files:
+        path_f = source_folder + "/" + f
+        timestamp = os.path.getmtime(path_f)
+        last_mod = datetime.datetime.fromtimestamp(timestamp)
+        year_num = last_mod.year
+        month_num = last_mod.month
+        copy_file_to_target_folder(path_f, target_base_folder, year_num, month_num, split_by_month=False,
+                                   backup_files=True, delete_source=True)
+
+
+if __name__ == "__main__":
+    source_path = str(sys.argv[1])
+    target_path = str(sys.argv[2])
+    organize_files(source_path, target_path)
 
 
 def create_month_folders(year_folder_path):
@@ -26,45 +65,25 @@ def create_month_folder(i, year_folder_path):
     return month_folder_path
 
 
-def organize_files(source_folder_path, target_year_folder_path):
-    source_exists = os.path.exists(source_folder_path)
-    if not source_exists:
-        msg = "Cannot organize files. Source path does not exist. Source=" + source_folder_path
-        print(msg)
-        return
-    if not os.path.exists(target_year_folder_path):
-        msg = "Created target year folder: " + target_year_folder_path
-        print(msg)
-        os.mkdir(target_year_folder_path)
-
-    all_files = os.listdir(source_folder_path)
-    filtered_files = []
-
-    for f in all_files:
-        f_lower = f.lower()
-        if f_lower.endswith(".jpg") or f_lower.endswith(".mp4") or f_lower.endswith(".mov") or f_lower.endswith(".png"):
-            filtered_files.append(f_lower)
-
-    for f in filtered_files:
-        path_f = source_folder_path + "/" + f
-        timestamp = os.path.getmtime(path_f)
-        last_mod = datetime.datetime.fromtimestamp(timestamp)
-
-        month_num = last_mod.month
-        copy_file_to_month_folder(path_f, target_year_folder_path, month_num, backup_files=True, delete_source=True)
-
-
-def copy_file_to_month_folder(source_file, target_year_folder_path, month_num, backup_files=False, delete_source=True):
-    month_folder_path = create_month_folder(month_num, target_year_folder_path)
+def copy_file_to_target_folder(source_file, target_base_folder_path, year_num, month_num, split_by_month=True,
+                               backup_files=False, delete_source=True):
     file_name = os.path.basename(source_file)
-    target_file_path = month_folder_path + "/" + file_name
+    year_folder_path = target_base_folder_path + "/" + str(year_num)
+    if not os.path.exists(year_folder_path):
+        os.mkdir(year_folder_path)
+
+    if split_by_month:
+        month_folder_path = create_month_folder(month_num, year_folder_path)
+        target_file_path = month_folder_path + "/" + file_name
+    else:
+        target_file_path = year_folder_path + "/" + file_name
 
     if not os.path.exists(target_file_path):
         shutil.copy2(source_file, target_file_path)
         print("{} copied to {}".format(file_name, target_file_path))
 
     if backup_files:
-        backup_folder_path = target_year_folder_path + "/backup"
+        backup_folder_path = target_base_folder_path + "/backup"
         if not os.path.exists(backup_folder_path):
             os.mkdir(backup_folder_path)
             msg = "\t\tCreated backup folder: {}".format(backup_folder_path)
